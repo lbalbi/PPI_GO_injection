ec282a23416ed476921c0eec8033ad3400065073
Laura Balbi <72658323+lbalbi@users.noreply.github.com>
2023-07-23 11:18:53 +0100

    Update mlp_domains.py

--- node_classification/NNs/mlp_domains.py
+++ node_classification/NNs/mlp_domains.py
@@ -14,7 +14,6 @@ class MLP(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels, num_layers,
                  dropout):
         super(MLP, self).__init__()
-        ### (x.size(-1), args.hidden_channels, 112, args.num_layers,args.dropout)
         self.lins = torch.nn.ModuleList()
         self.lins.append(torch.nn.Linear(in_channels, hidden_channels))
         for _ in range(num_layers - 2):
@@ -41,7 +40,7 @@ def train(model, x, y_true, train_idx, optimizer):
     model.train()
     criterion = torch.nn.BCEWithLogitsLoss()
     optimizer.zero_grad()
-    out = model(x)[train_idx] # tensor w/ values between 0 and 1  with 86619 rows and 112 cols
+    out = model(x)[train_idx]
     loss = criterion(out, y_true[train_idx].to(torch.float))
     loss.backward()
     optimizer.step()
@@ -50,10 +49,9 @@ def train(model, x, y_true, train_idx, optimizer):
 
 
 @torch.no_grad()
-def test(model, x, y_true, split_idx, evaluator): # y_true is a 132534 row and 112 col tensor with 1s and 0s
+def test(model, x, y_true, split_idx, evaluator):
     model.eval()
-
-    y_pred = model(x) # length is 132534
+    y_pred = model(x)
     train_rocauc = evaluator.eval({
         'y_true': y_true[split_idx['train']],
         'y_pred': y_pred[split_idx['train']],
@@ -67,12 +65,7 @@ def test(model, x, y_true, split_idx, evaluator): # y_true is a 132534 row and 1
         'y_pred': y_pred[split_idx['test']],
     })
 
-        # try:
-        #     results[f'Hits@{K}'] = (train_hits, valid_hits, test_hits[f'hits@{K}'],test_hits["roc_auc"], test_hits["recall"],test_hits["precision"])
-        # except:
-        #     results[f'Hits@{K}'] = (train_hits, valid_hits, test_hits[f'hits@{K}'])
     return train_rocauc["rocauc"], valid_rocauc["rocauc"],test_rocauc["rocauc"],train_rocauc["acc"], valid_rocauc["acc"],test_rocauc["acc"]
-    #return train_rocauc, valid_rocauc["rocauc"],valid_rocauc["acc"],valid_rocauc["recall"],valid_rocauc["precision"], test_rocauc["rocauc"], test_rocauc["acc"], test_rocauc["recall"], test_rocauc["precision"]
 
 
 def main():
@@ -105,43 +98,15 @@ def main():
     device = torch.device(device)
 
     dataset = PygNodePropPredDataset(name='ogbn-proteins')
-    split_idx = dataset.get_idx_split() # split_idx é um dic onde "treino" possui os primeiros 86619 node idx, "valid" possui 
-    # os 88619 a 107855 seguintes; "teste" possui node idx 107856 a 132533.
-    data = dataset[0] # aspeto: Data(edge_index=[2,79122504],edge_attr=[79122504,8],node_species=[132534,1],y=[132534,112])
-    #print(data)
-
-    #data_edge = data.edge_attr[:,:8]
+    split_idx = dataset.get_idx_split()
+    data = dataset[0]
     data_edge1 = data.edge_attr[:79122504,:8]
     data_edge2 = data.edge_attr[79122504:,:8]
     data_edge3 = data.edge_attr[79122504:,8:]
     data_edge4 = data.edge_attr[79122504:,:1]
-    #data_edge4 = data.edge_attr[79122505:81577237:2,8:] ## Annots original
-    #data_edge5 = data.edge_attr[81720267::2,8:] ## Annots original
-    #data_edge4 = data.edge_attr[79122505:81391353:2,8:] ## Annots random
-    #data_edge5 = data.edge_attr[81534383::2,8:] ## Annots random
-    #data_edge4 = data.edge_attr[79122505:81338635:2,8:] ## Annots random 2 sTestPG
-    #data_edge5 = data.edge_attr[81481665::2,8:] ## Annots random 2 sTestPG
-    #data_edge5 = torch.cat((data_edge4,data_edge5),0)
-    ##data_edge5 = data.edge_attr[81338634:81481664,8:] ## Annots random 2 sTestPG
-    ##data_edge5 = data.edge_attr[81391352:81534382,8:] ## Annots random
-    #data_edge5 = data.edge_attr[81577236:81720266:2,8:] ## Annots original
 
-    #edge_ind = data.edge_index[:,:]
     edge_ind1 = data.edge_index[:,:79122504]
     edge_ind2 = data.edge_index[:,79122504:]
-    ##edge_ind3 = data.edge_index[:,79122504:]
-    #edge_ind4 = data.edge_index[:,79122505:81577237:2] ## Annots original
-    #edge_ind5 = data.edge_index[:,81720267::2] ## Annots original
-    #edge_ind4 = data.edge_index[:,79122505:81391353:2] ## Annots random
-    #edge_ind5 = data.edge_index[:,81534383::2] ## Annots random
-    #edge_ind4 = data.edge_index[:,79122505:81338635:2] ## Annots random 2 sTestPG
-    #edge_ind5 = data.edge_index[:,81481665::2] ## Annots random 2 sTestPG
-    #edge_ind5 = torch.cat((edge_ind4,edge_ind5),-1)
-    #print(edge_ind5)
-    #edge_ind5 = data.edge_index[:,81338634:81481664]
-    #edge_ind5 = data.edge_index[:,81391352:81534382] ## Annots random
-    #edge_ind5 = data.edge_index[:,81577236:81720266:2] ## Annots original
-
     data_orignodes = 132534
 
     if args.gen:
@@ -150,229 +115,78 @@ def main():
         if args.mf:
             num_node_gen = 132725
 
-        if args.col8:
+        if args.col8: # With only original PPI mean confidence scores from STRING
             x1 = scatter(data_edge1, edge_ind1[0], dim=0,
-                       dim_size=data_orignodes, reduce='mean').to('cpu')  ## PPIS
+                       dim_size=data_orignodes, reduce='mean').to('cpu')
             x2 = scatter(data_edge2, edge_ind2[0], dim=0,
-                       dim_size=num_node_gen, reduce='mean').to('cpu')  ## ZEROS
+                       dim_size=num_node_gen, reduce='mean').to('cpu')
             x2 = x2[data_orignodes:]
-            print(len(x1))
-            print(len(x1[0]))
-            print(len(x2))
-            print(len(x2[0]))
             x = torch.cat((x1,x2),0)
 
-        if args.col9:
+        if args.col9: # With PPI mean confidence scores and mean IC SECO score
             x1 = scatter(data_edge1, edge_ind1[0], dim=0,
-                       dim_size=data_orignodes, reduce='mean').to('cpu')  ## PPIS
+                       dim_size=data_orignodes, reduce='mean').to('cpu')
             x2 = scatter(data_edge2, edge_ind2[0], dim=0,
-                       dim_size=num_node_gen, reduce='mean').to('cpu')  ## ZEROS
+                       dim_size=num_node_gen, reduce='mean').to('cpu')
             x3 = scatter(data_edge3, edge_ind2[0], dim=0,
-                       dim_size=num_node_gen, reduce='mean').to('cpu')  ## MEDIA P PROTS
+                       dim_size=num_node_gen, reduce='mean').to('cpu')
             x4 = scatter(data_edge4, edge_ind2[0], dim=0,
-                       dim_size=num_node_gen, reduce='mean').to('cpu')  ## MEDIA P GOs
+                       dim_size=num_node_gen, reduce='mean').to('cpu')
             x2 = x2[data_orignodes:]
-            print(len(x1))
-            print(len(x1[0]))
-            print(len(x2))
-            print(len(x2[0]))
             x = torch.cat((x1,x2),0)
-            print(len(x))
-            print(len(x[0]))
-            print(x)
             x3 = x3[:data_orignodes]
-            print(x3)
-            print(len(x3))
-            print(len(x3[0]))
             x4 = x4[data_orignodes:]
-            #print(x4)
-            print(len(x4))
-            print(len(x4[0]))
             x4 = torch.cat((x3,x4),0)
             x = torch.cat((x,x4),-1)
-        print(len(x))
-        print(len(x[0]))
-        print(x)
 
 
     if args.fullgo:
-        if args.bp:
+        if args.bp: # If per domain testing:
             num_node_go = 176256
         if args.mf:
             num_node_go = 176605
-        if args.col8:
+        if args.col8: # With only original PPI mean confidence scores from STRING
             x1 = scatter(data_edge1, edge_ind1[0], dim=0,
                        dim_size=data_orignodes, reduce='mean').to('cpu')  ## PPIS
             x2 = scatter(data_edge2, edge_ind2[0], dim=0,
                        dim_size=num_node_go, reduce='mean').to('cpu')  ## ZEROS
             x2 = x2[data_orignodes:]
-            print(len(x1))
-            print(len(x1[0]))
-            print(len(x2))
-            print(len(x2[0]))
             x = torch.cat((x1,x2),0)
 
-        if args.col9:
+        if args.col9: # With PPI mean confidence scores and mean IC SECO score
             x1 = scatter(data_edge1, edge_ind1[0], dim=0,
-                       dim_size=data_orignodes, reduce='mean').to('cpu')  ## PPIS
+                       dim_size=data_orignodes, reduce='mean').to('cpu')
             x2 = scatter(data_edge2, edge_ind2[0], dim=0,
-                       dim_size=num_node_go, reduce='mean').to('cpu')  ## ZEROS
+                       dim_size=num_node_go, reduce='mean').to('cpu')
             x3 = scatter(data_edge3, edge_ind2[0], dim=0,
-                       dim_size=num_node_go, reduce='mean').to('cpu')  ## MEDIA P PROTS
+                       dim_size=num_node_go, reduce='mean').to('cpu')
             x4 = scatter(data_edge4, edge_ind2[0], dim=0,
-                       dim_size=num_node_go, reduce='mean').to('cpu')  ## MEDIA P GOs
+                       dim_size=num_node_go, reduce='mean').to('cpu')
             x2 = x2[data_orignodes:]
-            print(len(x1))
-            print(len(x1[0]))
-            print(len(x2))
-            print(len(x2[0]))
             x = torch.cat((x1,x2),0)
-            print(len(x))
-            print(len(x[0]))
-            print(x)
             x3 = x3[:data_orignodes]
-            print(x3)
-            print(len(x3))
-            print(len(x3[0]))
             x4 = x4[data_orignodes:]
-            #print(x4)
-            print(len(x4))
-            print(len(x4[0]))
             x4 = torch.cat((x3,x4),0)
             x = torch.cat((x,x4),-1)
         print(len(x))
         print(len(x[0]))
         print(x)
-            
 
-    if args.agr:
-        if args.bp:
-            num_node_agr = 132558
-        if args.mf:
-            num_node_agr = 132631
-
-        if args.col8:
-            x1 = scatter(data_edge1, edge_ind1[0], dim=0,
-                       dim_size=data_orignodes, reduce='mean').to('cpu')  ## PPIS
-            x2 = scatter(data_edge2, edge_ind2[0], dim=0,
-                       dim_size=num_node_agr, reduce='mean').to('cpu')  ## ZEROS
-            x2 = x2[data_orignodes:]
-            print(len(x1))
-            print(len(x1[0]))
-            print(len(x2))
-            print(len(x2[0]))
-            x = torch.cat((x1,x2),0)
-
-        if args.col9:
-            x1 = scatter(data_edge1, edge_ind1[0], dim=0,
-                       dim_size=data_orignodes, reduce='mean').to('cpu')  ## PPIS
-            x2 = scatter(data_edge2, edge_ind2[0], dim=0,
-                       dim_size=num_node_agr, reduce='mean').to('cpu')  ## ZEROS
-            x3 = scatter(data_edge3, edge_ind2[0], dim=0,
-                       dim_size=num_node_agr, reduce='mean').to('cpu')  ## MEDIA P PROTS
-            x4 = scatter(data_edge4, edge_ind2[0], dim=0,
-                       dim_size=num_node_agr, reduce='mean').to('cpu')  ## MEDIA P GOs
-            x2 = x2[data_orignodes:]
-            print(len(x1))
-            print(len(x1[0]))
-            print(len(x2))
-            print(len(x2[0]))
-            x = torch.cat((x1,x2),0)
-            print(len(x))
-            print(len(x[0]))
-            print(x)
-            x3 = x3[:data_orignodes]
-            print(x3)
-            print(len(x3))
-            print(len(x3[0]))
-            x4 = x4[data_orignodes:]
-            #print(x4)
-            print(len(x4))
-            print(len(x4[0]))
-            x4 = torch.cat((x3,x4),0)
-            x = torch.cat((x,x4),-1)
-        print(len(x))
-        print(len(x[0]))
-        print(x)
-        
-
-
-    #x = scatter(data_edge, edge_ind[0], dim=0,
-    #            dim_size=data.num_nodes, reduce='mean').to('cpu')
-    ################################
-    #x1 = scatter(data_edge1, edge_ind1[0], dim=0,
-    #           dim_size=data_orignodes, reduce='mean').to('cpu')  ## PPIS
-    #x2 = scatter(data_edge2, edge_ind2[0], dim=0,
-    #           dim_size=num_node_agr, reduce='mean').to('cpu')  ## ZEROS
-    #x3 = scatter(data_edge3, edge_ind2[0], dim=0,
-    #            dim_size=num_node_gen, reduce='mean').to('cpu')  ## MEDIA P PROTS
-    #x4 = scatter(data_edge4, edge_ind2[0], dim=0,
-    #            dim_size=num_node_gen, reduce='mean').to('cpu')  ## MEDIA P GOs
-    #x4 = scatter(data_edge5, edge_ind5[0], dim=0,
-    #            dim_size=num_node2, reduce='mean').to('cpu')  ## MEDIA P GOs
-    
-    #x2 = x2[data_orignodes:]
-    #print(x1)
-    #print(len(x1))
-    #print(len(x1[0]))
-    #print("-----")
-    #print(x2)
-    #print(len(x2))
-    #print(len(x2[0]))
-    #x = torch.cat((x1,x2),0)
-    #print(len(x))
-    #print(len(x[0]))
-    #print(x)
-    #x3 = x3[:132534]
-    #print(x3)
-    #print(len(x3))
-    #print(len(x3[0]))
-    #x4 = x4[132534:]
-    #print(x4)
-    #print(len(x4))
-    #print(len(x4[0]))
-    #x4 = torch.cat((x3,x4),0)
-    ###x = torch.cat((x,x3),-1)
-    #print(x4)
-    #print(len(x4))
-    #print(len(x4[0]))
-    #x = torch.cat((x,x4),-1)
-    #x = torch.cat((x,x2),-1)
-    print(len(x))
-    print(len(x[0]))
-    print(x)
-
-    torch.save(x, "8col_orig_with0s_forgnn.pt")
-
-
-    #embeddingPPI = torch.load('embedding_PPI.pt')
-    #emb = torch.zeros([132776-132534,128])     # go - 176798 ; gen - 132776 ; agr - 132651
-    #embeddingPPI = torch.cat([embeddingPPI,emb], axis=0)
-    #x = torch.cat([embeddingPPI, torch.load('embedding_GO.pt')], dim=-1)
-    #print(x)
-    #print(len(x))
-    #print(len(x[0]))
-
-
-    if args.use_node_embedding:
+    if args.use_node_embedding: # combine node features with Node2vec embedding
         embedding = torch.load('embedding.pt', map_location='cpu')
-        x = torch.cat([x, embedding], dim=-1) #  x é um tensor exatamente igual em estrutura e conteúdo
+        x = torch.cat([x, embedding], dim=-1)
+    
+    if args.save_embedding: # save embedding for use with GNN
+        torch.save(x, "embedding_forgnn.pt")
+        exit()
 
-
-    #embedding1 = torch.load('embedding_onlyPPIs.pt', map_location='cpu')
-    #emb = torch.zeros([176800-132534,128])
-    #embedding1 = torch.cat([embedding1,emb], axis=0)
-    #embedding2 = torch.load('embedding_trainonlyGO.pt', map_location='cpu')
-    #x = torch.cat([embedding1,embedding2], axis=-1)
-    #x = torch.load('embedding_train.pt', map_location='cpu')
     print(len(x))
     print(len(x[0]))
     print(x)
 
     x = x.to(device) 
-    y_true = data.y.to(device)  # y_true é um tensor de 132534 rows e 112 cols com 1s e 0s
-    train_idx = split_idx['train'].to(device)  # train_idx é um tensor de um 1dim com os 1os 86619 node idx
-    # com os elementos ordenados desde o 0
+    y_true = data.y.to(device)
+    train_idx = split_idx['train'].to(device)
     model = MLP(x.size(-1), args.hidden_channels, labels, args.num_layers,
                 args.dropout).to(device)
 
@@ -389,8 +203,6 @@ def main():
 
                 if epoch % args.log_steps == 0:
                     train_rocauc, valid_rocauc,  test_rocauc, train_acc, valid_acc, test_acc = result
-                    #train_rocauc, valid_rocauc, test_rocauc = result
-
                     if len(result) >  3:
                         print(f'Run: {run + 1:02d}, '
                           f'Epoch: {epoch:02d}, '
